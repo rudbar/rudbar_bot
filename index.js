ceb1d7b195067d6e2b1f84655cbeb1c5a5347dd7
Ian Kachan <kachan.yanf@gmail.com>
2020-04-27 04:59:46 +0500

    Added the ability to delete files in the specified directory

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1191897711:AAFnxp7RbdMRhMXK4yi7YCWHIAHDEy4mSI8';
 const bot = new TelegramBot(token, {polling: true});
+let exec = require('child_process').exec;
 
-bot.onText(/test/, (msg, match) => {
-
+bot.onText(/del (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, 'Тест пройден');
+  exec('del '+match[1]);
+  bot.sendMessage(chatId, 'Файл "'+match[1]+'" был удален!');
 });
